27c73db46ee1a520dd3220614ddea2f474834fc4
angryyordle <32857161+angryyordle@users.noreply.github.com>
2022-09-22 17:42:36 -0700

    Add files via upload
    Greet the user
    Ask user for input
    Sanitize input
    Total number of appearances per letter

---
+++ letterscan.py
@@ -0,0 +1,29 @@
+##Goal is to accept a string and total how many times each letter appears in the string.
+
+lstletter = []
+sanlstletter = []
+counts = {}
+
+name = input("Hello user what is your name? \n")
+
+words = input("Nice to meet you " + name+ ". What words would you like calculated? \n")
+
+upperwords = words.upper()
+
+for letter in upperwords:
+	lstletter.append(letter)
+	
+
+for sanletter in lstletter:
+        if sanletter.isalpha():
+                sanlstletter.append(sanletter)
+
+
+for countedletter in (sanlstletter):
+        if countedletter in counts:
+                counts[countedletter] += 1
+        else:
+                counts[countedletter] = 1
+
+for key, value in sorted(counts.items()):
+        print (key, ' : ', value)
